a4c0dd0940de09d67d9656e4796ce486c2d2a11a
drmurtazakhan <drkhanmurtaza@gmail.com>
2024-10-02 10:26:26 +0400

    comments added

--- Graphics/Animation/Polygon Rotation Animation/PolygonRotateAnimation.java
+++ Graphics/Animation/Polygon Rotation Animation/PolygonRotateAnimation.java
@@ -1,4 +1,11 @@
 
+// GitHub repository of my Java Programs
+// https://github.com/drmurtazakhan/Java/
+
+// ResearchGate: http://www.researchgate.net/profile/Murtaza_Khan2/
+// LinkedIn: https://www.linkedin.com/in/dr-murtaza-khan-3b368019
+// Google Scholar: https://scholar.google.com/citations?user=n0JDQ0sAAAAJ
+
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
